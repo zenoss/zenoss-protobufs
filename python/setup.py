9d29497ce6d3c5e29e29897db12ca7a721000197
Danylo Kondratiev <knightpp@protonmail.com>
2022-06-20 13:30:19 +0300

    Update versions to v.1.4.0

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="zenoss-protobufs",
-    version="0.0.3",
+    version="1.4.0",
     packages=find_packages(),
     author="Zenoss",
     author_email="support@zenoss.com",
